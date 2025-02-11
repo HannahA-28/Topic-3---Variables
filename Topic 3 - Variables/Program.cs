89ef9efc7861f3cf95fc258e56665756624e6f25
Hannah Anderson <339729345@GoTVDSB.ca>
2025-02-11 14:44:27 -0500

    Onto question 2

--- Topic 3 - Variables/Program.cs
+++ Topic 3 - Variables/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            //String name, eyeColor, hairColor;
+            //int age, height, weight;
+
+            //name = "John Smith";
+            //age = 35; //not a lie
+            //height = 74; //inches
+            //weight = 180; //lbs
+            //eyeColor = "Blue";
+            //hairColor = "Brown";
+
+            //Console.WriteLine("Let's talk about " + name + ".");
+            //Console.WriteLine("He's " + height + " inches tall.");
+            //Console.WriteLine("He's " + weight + " pounds heavy.");
+            //Console.WriteLine("Actually, that's not too heavy.");
+            //Console.WriteLine("His eyes are " + eyeColor + " and hair is " + hairColor);
+            ////This line is tricky; try to get it exactly right.
+            //Console.WriteLine("If I add " + age + " , " + height + ", and " + weight
+            //    + " I get " + (age + height + weight) + ".");
+
+
+            String name, lastName, eyeColor, hairColor;
+            int age, height, weight;
+
+            name = "John";
+            lastName = " Smith";
+            age = 35; //not a lie
+            height = 74; //inches
+            weight = 180; //lbs
+            eyeColor = "Blue";
+            hairColor = "Brown";
+
+            Console.WriteLine("Let's talk about " + name + lastName + ".");
+            Console.WriteLine("He's " + height + " inches tall.");
+            Console.WriteLine("He's " + weight + " pounds heavy.");
+            Console.WriteLine("Actually, that's not too heavy.");
+            Console.WriteLine("His eyes are " + eyeColor + " and hair is " + hairColor);
+            //This line is tricky; try to get it exactly right.
+            Console.WriteLine("If I add " + age + " , " + height + ", and " + weight
+                + " I get " + (age + height + weight) + ".");
         }
     }
 }
