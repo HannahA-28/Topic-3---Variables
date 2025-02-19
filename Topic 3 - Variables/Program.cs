c907039d64fa5bc7615ee9e235910dd5a40db22b
Hannah Anderson <339729345@GoTVDSB.ca>
2025-02-19 14:09:10 -0500

    Finished

--- Topic 3 - Variables/Program.cs
+++ Topic 3 - Variables/Program.cs
@@ -24,25 +24,50 @@
             //    + " I get " + (age + height + weight) + ".");
 
 
-            String name, lastName, eyeColor, hairColor;
-            int age, height, weight;
+            //String name, lastName, eyeColor, hairColor;
+            //int age, height, weight;
 
-            name = "John";
-            lastName = " Smith";
-            age = 35; //not a lie
-            height = 74; //inches
-            weight = 180; //lbs
-            eyeColor = "Blue";
-            hairColor = "Brown";
+            //name = "John";
+            //lastName = " Smith";
+            //age = 35; //not a lie
+            //height = 74; //inches
+            //weight = 180; //lbs
+            //eyeColor = "Blue";
+            //hairColor = "Brown";
 
-            Console.WriteLine("Let's talk about " + name + lastName + ".");
-            Console.WriteLine("He's " + height + " inches tall.");
-            Console.WriteLine("He's " + weight + " pounds heavy.");
-            Console.WriteLine("Actually, that's not too heavy.");
-            Console.WriteLine("His eyes are " + eyeColor + " and hair is " + hairColor);
-            //This line is tricky; try to get it exactly right.
-            Console.WriteLine("If I add " + age + " , " + height + ", and " + weight
-                + " I get " + (age + height + weight) + ".");
+            //Console.WriteLine("Let's talk about " + name + lastName + ".");
+            //Console.WriteLine("He's " + height + " inches or (" + (height * 2.54) + " cm) tall.");
+            //Console.WriteLine("He's " + weight + " pounds or (" + (weight / 2.2) + "kg) heavy.");
+            //Console.WriteLine("Actually, that's not too heavy.");
+            //Console.WriteLine("His eyes are " + eyeColor + " and hair is " + hairColor);
+            ////This line is tricky; try to get it exactly right.
+            //Console.WriteLine("If I add " + age + " , " + height + ", and " + weight
+            //    + " I get " + (age + height + weight) + ".");
+
+            //string name = Jonathan;
+            //Console.WriteLine(name);
+            //name = "Jon";
+            //Console.WriteLine(name);
+
+            //int myNumber = "15";
+            //Console.WriteLine(myNumber);
+
+            //int myNumber = 15;
+            //myNumber = 20;
+            //Console.WriteLine(myNumber);
+
+            //double height = 1.62;
+            //Console.WriteLine(height);
+
+            //Console.WriteLine("The difference between a double and int is that a double has decimals while an int is a solid number");
+           
+            //Console.WriteLine(10 + 12.0);
+            //Console.WriteLine("Yes the computer can solve an int and double put together");
+
+            //int age = 15;
+            //Console.WriteLine(age / 2); 
+           
+
         }
     }
 }
